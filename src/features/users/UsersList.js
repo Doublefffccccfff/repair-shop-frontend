e1a7e57e419c0cf6f5f86385ce435d10d9ddfa7f
Ankit <dixit.ankit917@gmail.com>
2026-04-01 06:32:33 +0530

    first commit

---
+++ src/features/users/UsersList.js
@@ -0,0 +1,62 @@
+import { useGetUsersQuery } from "./usersApiSlice"
+import User from './User'
+import useTitle from "../../hooks/useTitle"
+
+const UsersList = () => {
+    useTitle('Users List')
+
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery('usersList', {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
+
+    let content
+
+    if (isLoading) content = <p>Loading...</p>
+
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th user__username">Username</th>
+                        <th scope="col" className="table__th user__roles">Roles</th>
+                        <th scope="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+            
+        )
+    }
+
+    // Move it here so it's always visible
+return (
+    <>
+        <h1>Users List</h1> 
+        {content}
+    </>
+)
+}
+export default UsersList
